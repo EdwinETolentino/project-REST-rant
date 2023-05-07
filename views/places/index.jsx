91a4c0dafae449fe001a19f85efd48f7a2733322
Edwin Tolentino <edwintolentino@Edwins-Macbook.local>
2023-05-06 17:37:03 -0700

    Added the edit functionality

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -2,28 +2,26 @@ const React = require("react");
 const Def = require("../default");
 
 function index(data) {
-  let placesFormatted = data.places.map((place, index) => {
+    let placesFormatted = data.places.map((place) => {
+        return (
+            <div className="col-sm-6">
+                <h2>
+                    <a href={`/places/${place.id}`}>
+                        {place.name}
+                    </a>
+                </h2>
+                ...
+            </div>
+        )
+    })
     return (
-      <div className='col-sm-6'>
-        <h2>
-          <a href={`/places/${index}`}>{place.name}</a>
-        </h2>
-        <p className='text-center'>{place.cuisines}</p>
-        <img src={place.pic} alt={place.name} />
-        <p className='text-center'>
-          Located in {place.city}, {place.state}
-        </p>
-      </div>
+        <Def>
+            <main>
+                <h1>PLACES INDEX PAGE</h1>
+                <div className='row'>{placesFormatted}</div>
+            </main>
+        </Def>
     );
-  });
-  return (
-    <Def>
-      <main>
-        <h1>PLACES INDEX PAGE</h1>
-        <div className='row'>{placesFormatted}</div>
-      </main>
-    </Def>
-  );
 }
 
 module.exports = index;
